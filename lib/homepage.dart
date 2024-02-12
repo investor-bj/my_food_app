ee25fd873bd292be5dce0fbeb2215d9e47346eb9
ChukwuemekaFN1715 <ogfrancisnwaefido@gmail.com>
2024-02-12 08:24:08 -0600

    initialized project

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,49 +1,14 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
-import 'package:my_food_app/unbodied.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
-    Future.delayed(
-      widgetFactory
-      const Duration(seconds: 50),
-      () => Navigator.push(
-          context, MaterialPageRoute(builder: (context) => const UnBodied())),
+    return const Scaffold(
+      body: Center(
+        child: Image(image: AssetImage("logo.jpg")),
+      ),
     );
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-          Positioned(
-            top: 20,
-            left: 20,
-            child: Image.asset(
-              'assets/images/top-position.png',
-              width: 20,
-              height: 100,
-            ),
-          ),
-          Text("Leye"),
-          Text('paulet'),
-          Center(
-            child: Image.asset(
-              'assets/images/dis.png',
-              width: 150,
-              height: 150,
-            ),
-          ),
-          Positioned(
-            bottom: 20,
-            right: 20,
-            child: Image.asset('assets/images/bottom.png',
-                width: 100, height: 100),
-          ),
-        ],
-      ), //center
-    ); //scaffold
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_food_app/homepage.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const MyHomePage()
     );
   }
 }
